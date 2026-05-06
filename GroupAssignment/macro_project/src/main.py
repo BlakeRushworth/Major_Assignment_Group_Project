359b17261edf5950cb904c1b7e00bd836204f8bb
Ashikur17 <ashikurahman2007@gmail.com>
2026-05-06 16:03:25 +1000

    Update main.py

--- GroupAssignment/macro_project/src/main.py
+++ GroupAssignment/macro_project/src/main.py
@@ -1,3 +1,10 @@
-hi = "stupid"
+from src.setup_check import run_setup
+from src.services.workflow_service import WorkflowService
 
-print(hi)
+def main() -> None:
+    run_setup()                         
+    workflow = WorkflowService()
+    workflow.run_full_pipeline()
+
+if __name__ == "__main__":
+    main()
