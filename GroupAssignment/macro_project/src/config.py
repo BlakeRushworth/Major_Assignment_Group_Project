d75f3d7311984d443be5606d1fedaa4dbf182166
Ashikur17 <ashikurahman2007@gmail.com>
2026-05-06 12:33:27 +1000

    Organize AppConfig with comments for clarity

--- GroupAssignment/macro_project/src/config.py
+++ GroupAssignment/macro_project/src/config.py
@@ -1,21 +1,20 @@
 from pathlib import Path
 
-
 class AppConfig:
-    
-
+    # Root Directories
     BASE_DIR = Path(__file__).resolve().parent.parent
     DATA_DIR = BASE_DIR / "data"
     RAW_DATA_DIR = DATA_DIR / "raw"
     DATESET_DIR = RAW_DATA_DIR / "stream_macroinvertebrates"
+    # Output Directories
     OUTPUTS_DIR = BASE_DIR / "outputs"
     EDA_OUTPUT_DIR = OUTPUTS_DIR / "eda"
     MODEL_OUTPUT_DIR = OUTPUTS_DIR / "models"
+    REPORT_OUTPUT_DIR = OUTPUTS_DIR / "reports"
+    # Image Settings
     IMAGE_SIZE = (128, 128)
     SUPPORTED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".bmp"}
 
-    
-    
     @classmethod
     def create_required_dir(cls):
         for path in [cls.DATA_DIR,
