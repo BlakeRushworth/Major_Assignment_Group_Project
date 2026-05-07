bad39421339fde0f8b719ff69249f3c63f4b7af2
BlakeRushworth <blake@bitbot.com.au>
2026-05-07 16:15:28 +1000

    more things broke

--- GroupAssignment/macro_project/src/dataset_index.py
+++ GroupAssignment/macro_project/src/dataset_index.py
@@ -1,4 +1,6 @@
+import sys
 from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import cv2
 import pandas as pd
 from src.config import RAW_DATA_DIR, SUPPORTED_EXTENSIONS
